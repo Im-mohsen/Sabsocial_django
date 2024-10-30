e2a4847debd05d90df5093c606accff58559cb7c
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-10-30 13:14:31 +0330

    add django-debug-toolbar

--- sabzsocial/settings.py
+++ sabzsocial/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.postgres',
     'easy_thumbnails',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -174,4 +176,6 @@ THUMBNAIL_DEBUG = True
 #     'social.user': lambda u: reverse_lazy("social:user_detail", args=[u.username])
 # }
 
-
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
