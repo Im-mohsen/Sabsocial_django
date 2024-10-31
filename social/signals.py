e4b087a8dbb7bd03bf329950142af33979d9ac1c
Im-Mohsen <mohsendarabi2003@yahoo.com>
2024-10-31 14:09:21 +0330

    add signal to complete register form

--- social/signals.py
+++ social/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import m2m_changed, post_delete
+from django.db.models.signals import m2m_changed, post_delete, post_save
 from django.dispatch import receiver
-from .models import Post
+from .models import Post,User
 from django.core.mail import send_mail
 
 
@@ -18,3 +18,16 @@ def post_delete_admin(sender, instance, **kwargs):
     subject = f"Your post has been deleted"
     message = f"Your post has been deleted (ID: {instance.id})"
     send_mail(subject, message, 'mohsendarabi20003@gmail.com', [author.email], fail_silently=False)
+
+@receiver(post_save, sender=User)
+def user_auto_complete(sender, instance, created, **kwargs):
+    # کار هایی که میخواهیم بعد از دریافت سیگنال انجام شود را مینویسیم
+    if created:
+        instance.first_name = 'نام شما'
+        instance.last_name = 'نام خانوادگی شما'
+        instance.email = 'enteryouremail@test.com'
+        instance.bio = 'در مورد خود توضیحات بنویسید'
+        instance.job = 'شغل را وارد کنید'
+        instance.birth_date = '2001-01-01'
+
+        instance.save()
